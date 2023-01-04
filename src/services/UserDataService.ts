9634e337f3372e9f27e2da4f3374b43b46f2797d
taetanapol <taetanapol01@gmail.com>
2023-01-05 03:34:57 +0700

    update path

---
+++ src/services/UserDataService.ts
@@ -0,0 +1,33 @@
+import http from "../http-common";
+
+class UserDataService {
+  getAll() {
+    return http.get("/tutorials");
+  }
+
+  get(id: number) {
+    return http.get(`/tutorials/${id}`);
+  }
+
+  create(data: string) {
+    return http.post("/tutorials", data);
+  }
+
+  update(id: number, data: string) {
+    return http.put(`/tutorials/${id}`, data);
+  }
+
+  delete(id: number) {
+    return http.delete(`/tutorials/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/tutorials`);
+  }
+
+  findByTitle(title: string) {
+    return http.get(`/tutorials?title=${title}`);
+  }
+}
+
+export default new UserDataService();
